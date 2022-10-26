d5ac0de0b926c476bc2f302bc0d4bf1e1fa15c97
IanMcbull <ianmugenya@gmail.com>
2022-10-26 12:10:47 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Getting the form
+const urlForm = document.querySelector('.url-form');
+
+
+const submitUrl = async(e) =>{
+   e.preventDefault(); 
+  // Check whether the user passed in a url
+  const url = document.querySelector('.url').value;
+  const request = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+  const data = await request.json();
+  const cardsContainer = document.querySelector('.url-shortened-cards');
+  cardsContainer.innerHTML += `<div class="card">
+  <p>${url}</p>
+  <div class="shortened-link-container">
+      <p>${data.result.short_link}</p>
+      <button class="shortened-link-button" type="button">Copy</button>
+  </div>
+</div>`
+  console.log('Form Submitted') 
+}
+
+urlForm.addEventListener('submit', submitUrl);
